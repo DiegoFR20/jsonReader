33b9e39d49d047e611b3079cc0b2735921ac6fad
DiegoFR <diegofreire8482@hotmail.com>
2020-09-04 17:18:21 -0300

    Criação via Post adicionado

--- src/main/java/com/resources/AgendaResource.java
+++ src/main/java/com/resources/AgendaResource.java
@@ -1,14 +1,17 @@
 package com.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.domain.Agenda;
 import com.dto.AgendaDTO;
@@ -28,13 +31,19 @@ public class AgendaResource {
 
 		return ResponseEntity.ok().body(listaDTO);
 	}
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<AgendaDTO> findById(@PathVariable Integer id) {
 		Agenda agenda = service.findById(id);
-		
-		
-				
+
 		return ResponseEntity.ok().body(new AgendaDTO(agenda));
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody AgendaDTO agendaDTO) {
+		Agenda agenda = service.fromDTO(agendaDTO);
+		agenda = service.insert(agenda);		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(agenda.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/services/AgendaService.java
+++ src/main/java/com/services/AgendaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domain.Agenda;
+import com.dto.AgendaDTO;
 import com.repository.AgendaRepository;
 import com.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class AgendaService {
 	public Agenda findById(Integer id) {
 		Optional<Agenda> agenda = repo.findById(id);
 		return agenda.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Agenda insert(Agenda agenda) {
+		return repo.insert(agenda);
+	}
+	
+	public Agenda fromDTO(AgendaDTO agendaDTO) {
+		return new Agenda(agendaDTO.getHash(), agendaDTO.getId(), agendaDTO.getNome(), agendaDTO.getEsfera());
 	}
 }
